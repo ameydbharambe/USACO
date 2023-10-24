e05a13f04c90f1234a8b09812c7273e541904dce
Amey <ameybharambe@gmail.com>
2023-10-23 21:40:47 -0700

    solved ez cowsignal problem today

--- cowsignal.py
+++ cowsignal.py
@@ -1,4 +1,20 @@
+import sys
+
+sys.stdin = open("cowsignal.in", "r")
+sys.stdout = open("cowsignal.out", "w")
 M, N, K = input().split()
 M = int(M)
 N = int(N)
 K = int(K)
+finalSignal = []
+for i in range(M):
+    signal = input()
+    signalArray = [char for char in signal]
+    finalSignal.append(signalArray)
+for i in range(M):
+    newList = []
+    for j in range(N):
+        for k in range(K):
+            newList.append(finalSignal[i][j])
+    for n in range(K):
+        print("".join(newList))
